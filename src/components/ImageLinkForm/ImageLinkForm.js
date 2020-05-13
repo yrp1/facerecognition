a414f2840056dc0579ef46f6ca27735656f73749
yrp1 <61746292+yrp1@users.noreply.github.com>
2020-05-13 16:44:07 -0400

    adding project files

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,19 @@
+import React from "react"
+import "./ImageLinkForm.css"
+
+const ImageLinkForm = () =>{
+	return(<div>
+			<p className="f3">
+				{"This Magic Brain will detect faces in your pictures. Give it a try..."} 
+			</p>
+				<div className="center">
+					<div className=" fan center pa4 br-3 shadow-2">
+						<input className="f4 w-70 center"type="text" placeholder="url..."/>
+						<button className=" ml3 w-30 grow f4 link ph3 pv2 dib white "> Detect </button>
+					</div>
+				</div>
+		</div>
+		)
+}
+
+export default ImageLinkForm
