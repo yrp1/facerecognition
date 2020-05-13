ba2a0af55d68df77700654fa8ffbc3ba71d9df44
yrp1 <61746292+yrp1@users.noreply.github.com>
2020-05-13 16:44:31 -0400

    Updates

---
+++ precache-manifest.478369ea26d05d21c7a6dd51e240f6fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41136e4bd88798d2ae8957f2e9dfcab1",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "e87842ad4598ca2aa870",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "81017f1c768c22c64567",
+    "url": "/facerecognition/static/css/main.c8961b2f.chunk.css"
+  },
+  {
+    "revision": "e87842ad4598ca2aa870",
+    "url": "/facerecognition/static/js/2.63645277.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/facerecognition/static/js/2.63645277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81017f1c768c22c64567",
+    "url": "/facerecognition/static/js/main.b7408168.chunk.js"
+  },
+  {
+    "revision": "7747f7a09a564ebd257a",
+    "url": "/facerecognition/static/js/runtime-main.8421dbec.js"
+  }
+]);
